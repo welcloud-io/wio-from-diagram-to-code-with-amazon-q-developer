1cfbb97a01baef594a1b61f70c9a0d62df0d3e88
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-03-23 10:19:06 +0100

    include prompts with tutorial choice output

--- _playground/init-playground.sh
+++ _playground/init-playground.sh
@@ -23,6 +23,9 @@ if [ $tutorial_choice != '5' ]; then
 fi
 
 if [ $tutorial_choice == '1' ]; then
+echo """
+-------------------------------------------------------------------------------
+"""
 echo "preparing playground for tutorial $tutorial_choice..."
 cp -r ../../tutorials-starting-points/feedback-app-code/* .
 cat << 'EOF' > MERMAID-DIAGRAMS.md
@@ -30,24 +33,120 @@ cat << 'EOF' > MERMAID-DIAGRAMS.md
 <PUT MERMAID CODE HERE AND CLICK ON PREVIEW AT THE RIGHT HAND CORNER>
 ```
 EOF
+cat << 'EOF' > APP.DRAWIO.XML
+<PUT GENERATED DIAGRAM HERE>
+EOF
 echo "Done."
 echo "Created files:"
-ls
+ls -1
+echo """
+-------------------------------------------------------------------------------
+$(tput bold)$(tput setaf 1)[From Code to Mermaid Diagram with Q Desktop] PROMPTS:$(tput sgr0)
+
+...Go to Q Desktop en enter
+
+$(tput bold)> @workspace can you generate a mermaid diagram of my application$(tput sgr0)
+
+...Copy/Paste prompt results in MERMAID-DIAGRAMS.md
+
+$(tput bold)> @workspace can you generate a mermaid sequence diagram of the application$(tput sgr0)
+
+...Copy/Paste prompt results in MERMAID-DIAGRAMS.md
+
+$(tput bold)> @workspace can you generate a mermaid class diagram of the application$(tput sgr0)
+
+...Copy/Paste prompt results in MERMAID-DIAGRAMS.md
+
+-------------------------------------------------------------------------------
+$(tput bold)$(tput setaf 1)[From Code to Drawio Diagram with Q Desktop] PROMPTS:$(tput sgr0)
+
+...Go to Q Desktop and enter
+
+$(tput bold)> @workspace generate a draw.io diagram in an xml format for this application (I want to use AWS 2024 Icons, lines should be orthogonal, dataflow from up to bottom)$(tput sgr0) 
+
+...Copy/Paste prompt results in APP.DRAWIO.XML (Open it with text editor)
+
+-------------------------------------------------------------------------------
+"""
 fi
 
 if [ $tutorial_choice == '2' ]; then
+echo """
+-------------------------------------------------------------------------------
+"""
 echo "preparing playground for tutorial $tutorial_choice..."
 echo "Done."
 echo "Folder should be empty"
-ls
+ls -l
+echo """
+-------------------------------------------------------------------------------
+$(tput bold)$(tput setaf 1)[From Mermaid Diagram to Code with Q Desktop] PROMPTS:$(tput sgr0)
+
+...Go to Q Desktop and enter
+
+$(tput bold)> /dev can you generate application files from this mermaid diagram (I want the code of the lambdas to be written in python and the infrastructure as code with the python cdk v2)
+graph TD  
+A[User] -->|HTTP GET /| B[API Gateway]  
+B -->|Invoke| C[Landing Page Function]  
+C -->|Return HTML| B  
+B -->|Return HTML| A  
+A -->|HTTP POST /feedbacks| B  
+B -->|Invoke| D[Send Feedback Function]  
+D -->|Write| E[(DynamoDB Table)]  
+D -->|Publish| F[SNS Topic]  
+F -->|Send Email| G[User Email]  
+subgraph AWS Cloud  
+B  
+C  
+D  
+E  
+F  
+end$(tput sgr0)
+
+...Deploy generated code in your folder with 'cdk deploy'
+-------------------------------------------------------------------------------
+"""
 fi
 
 if [ $tutorial_choice == '3' ]; then
+echo """
+-------------------------------------------------------------------------------
+"""
 echo "preparing playground for tutorial $tutorial_choice..."
 cp -r ../../tutorials-starting-points/feedback-app-diagram/* .
 echo "Done."
 echo "Created files:"
-ls
+ls -1
+echo """
+-------------------------------------------------------------------------------
+$(tput bold)$(tput setaf 1)[From Drawio Diagram to Code with Q Desktop] PROMPTS:$(tput sgr0)
+
+...Open drawio diagram in folder
+
+...Go to Q Desktop and enter
+
+$(tput bold)> /dev can you generate application from the drawio diagram (I want the code of the lambdas to be written in python and the infrastructure as code with the python cdk v2)$(tput sgr0)
+
+...Deploy generated code in your folder with 'cdk deploy'
+
+-------------------------------------------------------------------------------
+$(tput bold)$(tput setaf 1)[From Drawio Diagram to Code with Q CLI] PROMPTS:$(tput sgr0)
+
+...Open terminal and type:
+$> q chat
+
+$(tput bold)> can you generate application from the drawio diagram (I want the code of the lambdas to be written in python and the infrastructure as code with the python cdk v2)$(tput sgr0)
+
+...Say yes (y) to all
+
+$(tput bold)> modify the drawio diagram to split the architecture diagram into well defined cdk construts (use colors and legend)$(tput sgr0)
+
+...Say yes (y) to all
+
+...Deploy generated code in your folder with 'cdk deploy'
+
+-------------------------------------------------------------------------------
+"""
 fi
 
 if [ $tutorial_choice == '4' ]; then
@@ -58,7 +157,12 @@ cat << 'EOF' > app.drawio.xml
 EOF
 echo "Done."
 echo "Created files:"
-ls
+ls -l
+echo """
+- Tutorial 1 (From Code to Diagram with Drawio + Q Desktop) 
+> 
+
+"""
 fi
 
 if [ $tutorial_choice == '5' ]; then
