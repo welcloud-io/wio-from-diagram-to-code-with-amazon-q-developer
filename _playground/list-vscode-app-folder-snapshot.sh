7cb8c12fd0a83c75d04edb6e361d7115ff8ba8db
Olivier <o.lemaitre@welcloud.io>
2025-09-01 19:35:46 +0000

    remove unused scripts

--- _playground/list-vscode-app-folder-snapshot.sh
+++
@@ -1,3 +0,0 @@
-# /bin/bash
-
-ls -r ../../vscode-app-folder-snapshots/*
--- _playground/watch-diagram.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-q chat --accept-all "generate application"
-
-while inotifywait -e modify diagram.drawio; do
-    echo "File changed, executing command..."
-    q chat --accept-all "generate application"
-done
