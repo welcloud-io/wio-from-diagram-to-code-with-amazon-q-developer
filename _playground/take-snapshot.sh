d077ff38286d4283927ec0e911ebd387c2fa4457
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-10-18 18:20:15 +0200

    take snaphot with tutorial name in tutorial.metadata file

--- _playground/take-snapshot.sh
+++ _playground/take-snapshot.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-folder="../../tutorials/tutorial-generated-examples/vscode-app-folder-$(date +%Y-%m-%d-%H-%M-%S)"
+if [ -f tutorial.metadata ]; then
+  . ./tutorial.metadata
+  echo "$tutorial_name"
+else
+  if [ "$1" == "" ]; then
+    echo "usage: $0 <tutorial-name>"
+    exit
+  else
+    tutorial_name="$1"
+    echo "tutorial_name=$1" > tutorial.metadata
+  fi
+fi
 
-cp -r . $folder
+sample_folder="../../tutorials/tutorial-generated-examples/$tutorial_name"
+rm -rf $sample_folder
+mkdir -p $sample_folder
+
+if [ -d "$sample_folder" ]; then
+  echo $sample_folder
+  cp -r . "$sample_folder"
+  echo "snapshot taken"
+fi
